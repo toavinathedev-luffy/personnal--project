b621282209a10145c09333574ecd3353765bc7bb
toavinathefirst@gmail.com <toavinathefirst@gmail.com>
2022-08-05 12:35:06 +0300

    link the button elements to  js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+"use strict";
 console.log("hello world");
+var input=document.getElementById("input"), 
+number =document.querySelectorAll(".number div"), 
+operator =document.querySelectorAll(".operator div"),
+result =document.getElementById("result"),
+clear =document.getElementById("clear"),
+resultDisplayed=false;
