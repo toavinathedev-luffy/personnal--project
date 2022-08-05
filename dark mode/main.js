c0d73639f6ca975138b191f4246a9a4937fa7df7
toavinathefirst@gmail.com <toavinathefirst@gmail.com>
2022-08-05 23:21:56 +0300

    code success

--- dark mode/main.js
+++ dark mode/main.js
@@ -1,5 +1,7 @@
-let checkbox = document.getElementById('toggle');
+console.log("hello")
+let checkbox = document.getElementById("toggle");
 toggle.addEventListener("click", () =>{
   document.body.classList.toggle("dark-bg");
-  checkbox.classList.toggle("dark-checkbox");
+  checkbox.classList.toggle("dark-checkbox")
+  console.log("hello world")
 })
